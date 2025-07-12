11ddbe41eb2824aa18474cf17342eda54d15d7c5
Danilo Nascimento <danilo.278@hotmail.com>
2025-07-11 21:05:25 -0300

    Entrada e saida

---
+++ faculdade_trunfo/programa.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <locale.h>
+
+int main(){
+    setlocale(LC_ALL,"portuguese");
+
+
+
+    int idade, matricula;
+    float altura;
+    char nome[50];
+
+    printf("Digite o nome do aluno: \n");
+    scanf("%s", nome);
+
+    printf("Digite a sua altura: \n");
+    scanf("%f", &altura);
+
+    printf("Digite a sua idade: \n");
+    scanf("%d", &idade);
+
+    printf("Digite a sua matricula: \n");
+    scanf("%d", &matricula);
+
+    printf("Seu nome é: %s - Matricula: %d\n", nome, matricula);
+    printf("Sua altura é: %.2f , e voce tem: %d anos.", altura, idade);
+
+
+
+
+
+    return 0;
+}
